526f6a6e856025cdb1890e5254df96cf836b94f3
Michael <pulis.michael@gmail.com>
2019-08-22 10:36:33 -0600

    Added core files

---
+++ Slapstick/PlayerInput.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slapstick
+{
+    /// <summary>
+    /// Handles player input
+    /// </summary>
+    public class PlayerInput
+    {
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        public void Update(GameTime gameTime)
+        {
+            //if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            //    Exit();
+
+            // TODO: Add your update logic here
+        }
+    }
+}
