e1fddadab62ecc0279f45c4242f538662de4ffbd
Arindam Pani <arindampani@gmail.com>
2019-08-29 12:02:23 -0600

    merging animation

--- Slapstick/Celeb.cs
+++ Slapstick/Celeb.cs
@@ -26,34 +26,19 @@ namespace Slapstick
 
         private const int HEALTH_HEIGHT_ABOVE_CELEB = 100;
 
-        private Rectangle[] frames = new Rectangle[60];
-        private int frameCounter = 0;
-        public Rectangle currentFrame;
-        private double animTimer;
 
-        public Texture2D celebTexture;
-
-        public Celeb()
+        public Celeb(Texture2D tex, GraphicsDeviceManager gdm)
         {
-            for (int i = 0; i < 60; i++)
-            {
-                frames[i] = new Rectangle(75 * (i % 6), 150 * (i / 6), 75, 150);
-            }
-            currentFrame = frames[frameCounter];
-        }
-
-
-        public void Initialize(GraphicsDeviceManager gdm)
-        {
-            position = new Vector2((gdm.PreferredBackBufferWidth) / 2,
+            texture = tex;
+            position = new Vector2((gdm.PreferredBackBufferWidth - texture.Width)/2,
                700);
-
+            Debug.WriteLine("position: " + position.X);
         }
 
         public void LoadContent(ContentManager Content)
         {
             clearThroatSFX = Content.Load<SoundEffect>("Sounds/clear_throat");
-            celebTexture = Content.Load<Texture2D>("Images/Celeb_Wave_SpriteSheet");
+
             heart = Content.Load<Texture2D>("Images/heart");
             anger = Content.Load<Texture2D>("Images/anger");
 
@@ -62,10 +47,11 @@ namespace Slapstick
 
         public void Draw(SpriteBatch spriteBatch)
         {
+            spriteBatch.Draw(texture, position, Color.White);
 
-            spriteBatch.Draw(celebTexture,position,currentFrame, Color.White, 0.0f, new Vector2(0, 0), 1.3f,SpriteEffects.None, 0.0f);
+            
 
-            if (GameState.Lives == 3)
+            if(GameState.Lives == 3)
             {
                 spriteBatch.Draw(heart, new Vector2(position.X - textureWidthDifference, position.Y - HEALTH_HEIGHT_ABOVE_CELEB), Color.White);
                 spriteBatch.Draw(heart, new Vector2(position.X, position.Y - HEALTH_HEIGHT_ABOVE_CELEB), Color.White);
@@ -92,24 +78,6 @@ namespace Slapstick
 
         }
 
-
-
-        public void update(GameTime gameTime)
-        {
-            animTimer += gameTime.ElapsedGameTime.TotalSeconds;
-
-            if (animTimer > .0333)
-            {
-                frameCounter++;
-                if (frameCounter >= 60)
-                {
-                    frameCounter = 0;
-                }
-                currentFrame = frames[frameCounter];
-                animTimer = 0;
-            }
-        }
-
         public void collision(bool isNoisy)
         {
             if (isNoisy)
@@ -125,7 +93,7 @@ namespace Slapstick
 
         public float getCenterX()
         {
-            return position.X + (75 / 2);
+            return position.X + (150 / 2);
         }
 
 
